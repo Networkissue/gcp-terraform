5fa2d1d0ca28138b1139b5b852aef77dfed3b68a
Rohith-Elatecare <rohith.vaddepally@aptcare.ai>
2025-04-14 19:57:49 +0530

    Updated errors

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 #gcp bucket
 
+provider "google" {
+  project = "saugust-tangent-456305-e4"
+  region  = "us-central1" # change based on your actual region
+}
+
+
 resource "google_storage_bucket" "my-bucket" {
   name                     = "gcp-git-bucket"
   project                  = "august-tangent-456305-e4"
